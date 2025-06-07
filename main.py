13ee5e998747b8cb8dceac23656c42d1c236469a
James <80779630+JamesParrott@users.noreply.github.com>
2025-06-07 09:24:19 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from cowsay import cowsay
+
+message = """
+The most remarkable thing about my mother is that for thirty years she served
+the family nothing but leftovers.  The original meal has never been found.
+		-- Calvin Trillin
+""".strip()
+
+def main():
+    print(cowsay(message))
+
+if __name__ == '__main__':
+    main()
